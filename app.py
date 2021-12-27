fdd0e5d1b4fa08837076e9f1239c86b70dd32ebb
michael241137 <92925912+michael241137@users.noreply.github.com>
2021-12-27 17:02:43 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
 from flask import Flask, redirect, url_for
-
+from flask import render_template
 app = Flask(__name__)
-@app.route('/home_page')
+@app.route('/Home')
 @app.route('/')
-def hello_world():
-    return 'Welcome to the main page!'
+def home_page():
+    found = True
+    if found:
+        return render_template('index.html', name = 'Michael')
+    else:
+        return render_template('index.html')
 
 @app.route('/about')
 def about_func():
-    return redirect('/catalog')
+    return render_template('about.html', profile = {'name':'Michael', 'second_name':'Alfasy'}, university = 'BGU',
+                          degrees = ['BSc','MSc'],
+                           hobbies = ('sport' , 'music', 'python', 'flask'))
 
 @app.route('/catalog')
 def catalog_func():
-    return redirect(url_for('hello_world'))
+    return render_template('catalog.html')
+
+@app.route('/assignment8')
+def assighn_func():
+    hobbies = ('sport', 'music', 'python', 'flask')
+    return render_template('assignment8.html')
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
